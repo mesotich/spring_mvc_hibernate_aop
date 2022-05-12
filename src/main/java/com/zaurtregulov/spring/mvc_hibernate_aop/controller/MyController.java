2448d155c0c6674d5a1e2598336be5920341aea2
Dmitry Shishkin <4144442@bk.ru>
2022-05-12 17:22:43 +0300

    @Service

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -3,6 +3,7 @@ package com.zaurtregulov.spring.mvc_hibernate_aop.controller;
 
 import com.zaurtregulov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import com.zaurtregulov.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +15,11 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String showAllEmployees(Model model) {
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps",allEmployees);
         return "all-employees";
     }
